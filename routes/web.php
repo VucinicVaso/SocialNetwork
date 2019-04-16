1d690a3417448c1a79e6ee7de9e662e193425907
VucinicVaso <vasoct@gmail.com>
2019-04-16 22:06:31 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,74 @@
 <?php
-Route::get('/', 'PagesController@index');
+/* Pages (index, login, register) */
+Route::get('/', 'PagesController@index')->name('/');
 
+/* Facebook */
+Route::get('/home', 'FacebookController@index')->name('home');
+
+/* Posts */
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('post/show/{id}', 'PostsController@show')->name('post/show');
+	Route::post('post/store', 'PostsController@store')->name('post/store');
+	Route::delete('post/destroy/{id}', 'PostsController@destroy');
+});
+
+/* Comments */
+Route::group(['middleware' => ['auth']], function () {
+	Route::post('comment/store', 'CommentsController@store')->name('comment/store');
+});
+
+/* Likes */
+Route::group(['middleware' => ['auth']], function () {
+	Route::post('like/store', 'LikesController@store');
+	Route::post('like/destroy', 'LikesController@destroy');
+});
+
+/* Messages */
+
+/* Notifications */
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('notifications/index/{type}', 'NotificationsController@index');
+	Route::get('notifications/{type}', 'NotificationsController@show');
+});
+
+/* Interests */
+
+/* Gallery */
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('gallery', 'GalleryController@index')->name('gallery');
+	Route::get('gallery/{id}', 'GalleryController@show')->name('gallery');
+	Route::post('gallery/store', 'GalleryController@store')->name('gallery/store');
+});
+
+/* Photo */
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('photo/{id}', 'PhotosController@show');
+	Route::post('photo/update/{type}', 'PhotosController@update');
+	Route::post('photo/store', 'PhotosController@store')->name('photo/store');
+	Route::delete('photo/destroy/{id}', 'PhotosController@destroy');
+});
+
+/* Friends */
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('friends/index/{type}', 'FriendsController@index')->name('friends/index');
+	Route::get('friends/show', 'FriendsController@show')->name('friends/show');
+	Route::get('friends/get', 'FriendsController@get')->name('friends/get');
+	Route::put('friends/update', 'FriendsController@update')->name('friends/update');
+	Route::post('friends/store', 'FriendsController@store')->name('friends/store');
+	Route::delete('friends/destroy', 'FriendsController@destroy')->name('friends/destroy');
+});
+
+/* profile */
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('profile/edit', 'ProfileController@edit')->name('profile/edit');
+	Route::put('profile/update/{type}', 'ProfileController@update')->where('type', '[A-Za-z]+');;
+});
+
+/* User */
 Auth::routes();
+Route::get('profile', 'HomeController@index')->name('profile');
+Route::get('/profile/about', 'HomeController@about')->name('profile/about');
+Route::get('/{name}/{user}', 'HomeController@show');
 
-Route::get('/profile', 'HomeController@index')->name('profile');
+
+
