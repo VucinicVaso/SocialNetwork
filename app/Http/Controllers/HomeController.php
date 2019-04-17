6d3a8eed22671abaebde3d329c4e25ec49ee5d63
VucinicVaso <vasoct@gmail.com>
2019-04-17 10:55:12 +0200

    edit HomeContoller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,7 +34,7 @@ class HomeController extends Controller
     /* friends profile */
     public function show($name, $id)
     {
-        $user     = User::with('profile')->get()->where('id', $id)->first();
+        $user     = User::find($id);
         $posts    = Post::with('user')->with('comments')->with('likes')->where('user_id', $user->id)->orderBy('id', 'DESC')->get();
         $friends  = Friend::friendsList($user->id);
         $isFriend = Friend::where('friend_id', auth()->user()->id)->where('user_id', $user->id)
