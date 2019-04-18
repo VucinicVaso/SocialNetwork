d886942d00357a1f1515674ecf27ea43c05079b6
VucinicVaso <vasoct@gmail.com>
2019-04-18 18:12:55 +0200

    edit users pages

--- resources/views/users/includes/post-modal.blade.php
+++ resources/views/users/includes/post-modal.blade.php
@@ -28,7 +28,7 @@ $(document).ready(function(){
                         <!-- post images -->
                     `;
                 if(images.length == 1){
-                    postData += `<img src="{{ url('storage/images') }}/${images[0]}" class="img-fluid w-100" style="height:500px;">`;
+                    postData += `<img src="${ADDRESS}/storage/images/${images[0]}" class="img-fluid w-100" style="height:500px;">`;
                 }else {
                     postData += `
                         <div id="demo" class="carousel slide" data-ride="carousel">
@@ -41,13 +41,13 @@ $(document).ready(function(){
 
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img src="{{ url('storage/images') }}/${images[0]}" class="img-fluid w-100" style="height:500px;">
+                                    <img src="${ADDRESS}/storage/images/${images[0]}" class="img-fluid w-100" style="height:500px;">
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="{{ url('storage/images') }}/${images[1]}" class="img-fluid w-100" style="height:500px;">
+                                    <img src="${ADDRESS}/storage/images/${images[1]}" class="img-fluid w-100" style="height:500px;">
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="{{ url('storage/images') }}/${images[2]}" class="img-fluid w-100" style="height:500px;">
+                                    <img src="${ADDRESS}/storage/images/${images[2]}" class="img-fluid w-100" style="height:500px;">
                                 </div>
                             </div>
 
@@ -66,8 +66,8 @@ $(document).ready(function(){
                             <!-- user and post data -->
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                             <div class="d-flex flex-row justify-content-start mt-1">
-                                <img src="{{ url('storage/images') }}/${post.user.profile_image}" class="p-2 img-fluid" style="height:75px;">
-                                <a href="#" class="p-2">${post.user.firstname} ${post.user.lastname}</a>
+                                <img src="${ADDRESS}/storage/images/${post.user.profile_image}" class="p-2 img-fluid" style="height:75px;">
+                                <a href="${ADDRESS}/${post.user.firstname}.${post.user.lastname}/${post.user.id}" class="p-2">${post.user.firstname} ${post.user.lastname}</a>
                             </div>
                             <small>${post.created_at}</small>
                             <p class="pt-2">${post.body}</p>
@@ -81,7 +81,7 @@ $(document).ready(function(){
                                 <form action="{{ route('comment/store') }}" method="POST">
                                 @csrf
                                 <div class="form-group row">
-                                    <img src="{{ url('storage/images') }}/${post.user.profile_image}" class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6 w-100 img-fluid rounded" style="height: 40px;">
+                                    <img src="${ADDRESS}/storage/images/${post.user.profile_image}" class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6 w-100 img-fluid rounded" style="height: 40px;">
                                     <input type="text" name="comment" class="col-xl-8 col-lg-8 col-md-8 col-sm-6 col-6 form-control mt-1" placeholder="Write a comment...">
                                     <input type="hidden" name="post_id" value="${post.id}">
                                 </div>                                  
@@ -96,9 +96,9 @@ $(document).ready(function(){
                         postData += comments.map(comment => {
                             return(`
                                 <div class="row pb-1">
-                                    <img src="{{ url('storage/images') }}/${comment.profile_image}" alt="John Doe" class="col-xl-2 col-lg-2 col-md-2 col-sm-4 col-4  w-100 img-fluid rounded" style="height: 50px;">
+                                    <img src="${ADDRESS}/storage/images/${comment.profile_image}" alt="John Doe" class="col-xl-2 col-lg-2 col-md-2 col-sm-4 col-4 w-100 img-fluid rounded" style="height: 50px;">
                                     <p class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-                                        <a href="{{ url('') }}/${comment.firstname}.${comment.lastname}/${comment.user_id}">
+                                        <a href="${ADDRESS}/${comment.firstname}.${comment.lastname}/${comment.user_id}">
                                             <b>${comment.firstname} ${comment.lastname}</b>
                                         </a>
                                         <small class='float-left'>${comment.created_at}</small>
