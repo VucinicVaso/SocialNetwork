0318080ffa7180aa5ddd58beaa707fa10db09cae
VucinicVaso <vasoct@gmail.com>
2019-04-18 19:00:09 +0200

    edit users files

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -32,8 +32,8 @@
                         <form action="{{ url('profile/update/user') }}" method="POST">
                             @csrf
                             <div class="form-group row">
-                                <label for="firstname" class="col-md-4">Firstname:</label>
-                                <input type="text" name="firstname" class="form-control col-md-8 {{ $errors->has('firstname') ? ' is-invalid' : '' }}" value="{{ auth()->user()->firstname }}">
+                                <label for="firstname" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">Firstname:</label>
+                                <input type="text" name="firstname" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12 {{ $errors->has('firstname') ? ' is-invalid' : '' }}" value="{{ auth()->user()->firstname }}">
                                  @if ($errors->has('firstname'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('firstname') }}</strong>
@@ -41,8 +41,8 @@
                                 @endif 
                             </div>
                             <div class="form-group row">
-                                <label for="lastname" class="col-md-4">Lastname:</label>
-                                <input type="text" name="lastname" class="form-control col-md-8 {{ $errors->has('lastname') ? ' is-invalid' : '' }}" value="{{ auth()->user()->lastname }}">
+                                <label for="lastname" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">Lastname:</label>
+                                <input type="text" name="lastname" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12 {{ $errors->has('lastname') ? ' is-invalid' : '' }}" value="{{ auth()->user()->lastname }}">
                                  @if ($errors->has('lastname'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('lastname') }}</strong>
@@ -50,8 +50,8 @@
                                 @endif 
                             </div>
                             <div class="form-group row">
-                                <label for="email" class="col-md-4">Email:</label>
-                                <input type="text" name="email" class="form-control col-md-8 {{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ auth()->user()->email }}">
+                                <label for="email" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">Email:</label>
+                                <input type="text" name="email" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12 {{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ auth()->user()->email }}">
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('day') }}</strong>
@@ -59,8 +59,8 @@
                                 @endif 
                             </div>
                             <div class="form-group row">
-                                <label for="date" class="col-md-3 col-form-label">Birthday</label>
-                                <div class="form-group col-md-3 col-sm-12">
+                                <label for="date" class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 col-form-label">Birthday</label>
+                                <div class="form-group col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
                                     <select class="form-control{{ $errors->has('day') ? ' is-invalid' : '' }}" id="day" name="day">
                                         <option value="">DAY</option>
                                     @for($i = 31; $i >= 1; $i--)
@@ -73,7 +73,7 @@
                                     </span>
                                 @endif  
                                 </div>                             
-                                <div class="form-group col-md-3 col-sm-12">
+                                <div class="form-group col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
                                     <select class="form-control{{ $errors->has('month') ? ' is-invalid' : '' }}" id="month" name="month">
                                         <option value="">MONTH</option>
                                     @for($i = 12; $i >= 1; $i--)
@@ -86,7 +86,7 @@
                                     </span>
                                 @endif  
                                 </div> 
-                                <div class="form-group col-md-3 col-sm-12">
+                                <div class="form-group col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
                                     <select class="form-control{{ $errors->has('year') ? ' is-invalid' : '' }}" id="year" name="year">
                                         <option value="">YEAR</option>
                                     @for($i = 2019; $i >= 1920; $i--)
@@ -101,28 +101,28 @@
                                 </div>                                                                        
                             </div>
                             <div class="form-group row justify-content-left"> 
-                                <label for="gender" class="col-md-4 col-form-label">Gender</label>
+                                <label for="gender" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 col-form-label">Gender</label>
                             @if(auth()->user()->gender === "female")
-                               <div class="form-check col-md-4">
+                               <div class="form-check col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 text-center">
                                     <label class="form-check-label">
                                     <input type="radio" class="form-check-input" name="gender" value="female" checked>Female
                                     </label>
                                 </div>                         
                             @else
-                                <div class="form-check col-md-4">
+                                <div class="form-check col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 text-center">
                                     <label class="form-check-label">
                                     <input type="radio" class="form-check-input" name="gender" value="female">Female
                                     </label>
                                 </div>                                 
                             @endif         
                             @if(auth()->user()->gender === "male")
-                               <div class="form-check col-md-4">
+                               <div class="form-check col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 text-center">
                                     <label class="form-check-label">
                                         <input type="radio" class="form-check-input" name="gender" value="male" checked>Male
                                     </label>
                                 </div>                         
                             @else
-                                <div class="form-check col-md-4">
+                                <div class="form-check col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 text-center">
                                     <label class="form-check-label">
                                         <input type="radio" class="form-check-input" name="gender" value="male">Male
                                     </label>
@@ -135,8 +135,8 @@
                             @endif
                             </div>
                             <div class="form-group row">
-                                <label for="city" class="col-md-4">City:</label>
-                                <input type="text" name="city" class="form-control{{ $errors->has('city') ? ' is-invalid' : '' }} col-md-8" value="{{ auth()->user()->city }}">
+                                <label for="city" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">City:</label>
+                                <input type="text" name="city" class="form-control{{ $errors->has('city') ? ' is-invalid' : '' }} col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12" value="{{ auth()->user()->city }}">
                             @if ($errors->has('city'))
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('city') }}</strong>
@@ -144,8 +144,8 @@
                             @endif 
                             </div>
                             <div class="form-group row">
-                                <label for="country" class="col-md-4">Country:</label>
-                                <input type="text" name="country" class="form-control{{ $errors->has('country') ? ' is-invalid' : '' }} col-md-8" value="{{ auth()->user()->country }}">
+                                <label for="country" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">Country:</label>
+                                <input type="text" name="country" class="form-control{{ $errors->has('country') ? ' is-invalid' : '' }} col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12d-8" value="{{ auth()->user()->country }}">
                            @if ($errors->has('country'))
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('country') }}</strong>
@@ -153,12 +153,12 @@
                             @endif
                             </div>
                             <div class="form-group row">
-                                <label for="status" class="col-md-4">Status:</label>
-                                <input type="text" name="status" class="form-control col-md-8" value="{{ auth()->user()->status }}">
+                                <label for="status" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">Status:</label>
+                                <input type="text" name="status" class="form-control col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12" value="{{ auth()->user()->status }}">
                             </div>
                             <div class="form-group row">
-                                <label for="bio" class="col-md-4">Biography:</label>
-                                <textarea class="form-control{{ $errors->has('bio') ? ' is-invalid' : '' }} col-md-8" name="bio">{{ auth()->user()->bio }}</textarea>
+                                <label for="bio" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">Biography:</label>
+                                <textarea class="form-control{{ $errors->has('bio') ? ' is-invalid' : '' }} col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12" name="bio">{{ auth()->user()->bio }}</textarea>
                            @if ($errors->has('bio'))
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('bio') }}</strong>
@@ -176,8 +176,8 @@
                         <form action="{{ url('profile/update/password') }}" method="POST">
                             @csrf
                             <div class="form-group row">
-                                <label for="password" class="col-md-4">Old Password:</label>
-                                <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }} col-md-8">
+                                <label for="password" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">Old Password:</label>
+                                <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }} col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                             @if ($errors->has('password'))
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('password') }}</strong>
@@ -185,8 +185,8 @@
                             @endif 
                             </div>
                             <div class="form-group row">
-                                <label for="newpassword" class="col-md-4">New Password:</label>
-                                <input type="password" name="newpassword" class="form-control{{ $errors->has('newpassword') ? ' is-invalid' : '' }} col-md-8">
+                                <label for="newpassword" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">New Password:</label>
+                                <input type="password" name="newpassword" class="form-control{{ $errors->has('newpassword') ? ' is-invalid' : '' }} col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                             @if ($errors->has('newpassword'))
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('newpassword') }}</strong>
@@ -194,8 +194,8 @@
                             @endif 
                             </div>
                             <div class="form-group row">
-                                <label for="confirmpassword" class="col-md-4">Confirm Password:</label>
-                                <input type="password" name="confirmpassword" class="form-control{{ $errors->has('confirmpassword') ? ' is-invalid' : '' }} col-md-8">
+                                <label for="confirmpassword" class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">Confirm Password:</label>
+                                <input type="password" name="confirmpassword" class="form-control{{ $errors->has('confirmpassword') ? ' is-invalid' : '' }} col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                             @if ($errors->has('confirmpassword'))
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('confirmpassword') }}</strong>
