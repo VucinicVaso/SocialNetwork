fb6d97e1f0cd842289e4227a09491925a15e94e0
VucinicVaso <vasoct@gmail.com>
2019-04-19 12:04:04 +0200

    edit users/show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,12 +8,12 @@
 
     <div class="row justify-content-center mt-2">
         
-        <div class="col-md-4">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
             <!-- sidebar -->
             @include('users.show-includes.sidebar')
         </div>
 
-        <div class="col-md-6">
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
 
             <!-- posts -->
             @include('users.show-includes.posts')
