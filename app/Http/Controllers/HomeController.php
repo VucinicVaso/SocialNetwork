0d5fd7aa909cdaa389652007bc9bea08771f446f
VucinicVaso <vasoct@gmail.com>
2019-04-19 12:35:26 +0200

    edit HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,20 +34,24 @@ class HomeController extends Controller
     /* friends profile */
     public function show($name, $id)
     {
-        $user     = User::findOrFail($id);
-        $posts    = Post::with('user')->with('comments')->with('likes')->where('user_id', $user->id)->orderBy('id', 'DESC')->get();
-        $friends  = Friend::friendsList($user->id);
-        $isFriend = Friend::where('friend_id', auth()->user()->id)->where('user_id', $user->id)
-                    ->orWhere('friend_id', $user->id)->where('user_id', auth()->user()->id)->first();
-           
-        $data = [
-            'user'     => $user,
-            'posts'    => $posts,
-            'friends'  => $friends,
-            'isFriend' => $isFriend
-        ];
+        if(auth()->user()->id == $id) {
+            return redirect('profile'); 
+        }else {
+            $user     = User::findOrFail($id);
+            $posts    = Post::with('user')->with('comments')->with('likes')->where('user_id', $user->id)->orderBy('id', 'DESC')->get();
+            $friends  = Friend::friendsList($user->id);
+            $isFriend = Friend::where('friend_id', auth()->user()->id)->where('user_id', $user->id)
+                        ->orWhere('friend_id', $user->id)->where('user_id', auth()->user()->id)->first();
+               
+            $data = [
+                'user'     => $user,
+                'posts'    => $posts,
+                'friends'  => $friends,
+                'isFriend' => $isFriend
+            ];
 
-        return view('users.show')->with($data);        
+            return view('users.show')->with($data);     
+        }   
     }
 
     /* about user page */
