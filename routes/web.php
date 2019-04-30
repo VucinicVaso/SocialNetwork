f111871f1277de60709c1b121f8203b92d8b6426
VucinicVaso <vasoct@gmail.com>
2019-04-30 18:49:48 +0200

    create RevalidateBackHistory middleware for routes

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,23 @@
 /* Pages (index, login, register) */
 Route::get('/', 'PagesController@index')->name('/');
 
-/* Facebook */
+/* Network */
 Route::get('/home', 'NetworkController@index')->name('home');
 
 /* Posts */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
 	Route::get('post/show/{id}', 'PostsController@show')->name('post/show');
 	Route::post('post/store', 'PostsController@store')->name('post/store');
 	Route::delete('post/destroy/{post}', 'PostsController@destroy');
 });
 
 /* Comments */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
 	Route::post('comment/store', 'CommentsController@store')->name('comment/store');
 });
 
 /* Likes */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
 	Route::post('like/store', 'LikesController@store');
 	Route::post('like/destroy', 'LikesController@destroy');
 });
@@ -28,20 +28,20 @@ Route::group(['middleware' => ['auth']], function () {
 /* Messages */
 
 /* Notifications */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
 	Route::get('notifications/index/{type}', 'NotificationsController@index');
 	Route::get('notifications/{notification}', 'NotificationsController@show');
 });
 
 /* Gallery */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
 	Route::get('gallery', 'GalleryController@index')->name('gallery');
 	Route::get('gallery/{id}', 'GalleryController@show')->name('gallery');
 	Route::post('gallery/store', 'GalleryController@store')->name('gallery/store');
 });
 
 /* Photo */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
 	Route::get('photo/{id}', 'PhotosController@show');
 	Route::post('photo/update/{type}', 'PhotosController@update');
 	Route::post('photo/store', 'PhotosController@store')->name('photo/store');
@@ -49,7 +49,7 @@ Route::group(['middleware' => ['auth']], function () {
 });
 
 /* Friends */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
 	Route::get('friends/index/{type}', 'FriendsController@index')->name('friends/index');
 	Route::get('friends/show', 'FriendsController@show')->name('friends/show');
 	Route::get('friends/get', 'FriendsController@get')->name('friends/get');
@@ -59,16 +59,16 @@ Route::group(['middleware' => ['auth']], function () {
 });
 
 /* profile */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
 	Route::get('profile/edit', 'ProfileController@edit')->name('profile/edit');
 	Route::put('profile/update/{type}', 'ProfileController@update')->where('type', '[A-Za-z]+');;
 });
 
 /* User */
 Auth::routes();
-Route::get('profile', 'HomeController@index')->name('profile');
-Route::get('/profile/about', 'HomeController@about')->name('profile/about');
-Route::get('/{name}/{user}', 'HomeController@show');
 
-
-
+Route::group(['middleware' => ['auth', 'revalidate']], function () {
+	Route::get('profile', 'HomeController@index')->name('profile');
+	Route::get('/profile/about', 'HomeController@about')->name('profile/about');
+	Route::get('/{name}/{user}', 'HomeController@show');
+});
